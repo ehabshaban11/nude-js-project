fe9934cf4a95ce8515c0deafbb1d8b9865a89c90
ehabshaban11 <141747535+ehabshaban11@users.noreply.github.com>
2025-03-06 01:40:51 +0200

    Update server.js
    "הוספת בדיקת חיבור למסד הנתונים MySQL"

--- server.js
+++ server.js
@@ -1,35 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(cors());
-app.use(express.json());  
-app.use(express.urlencoded({ extended: true })); 
-
-const userRouters = require('./routes/userRoutes');
-const measurementRoutes = require('./routes/measurementRoutes');
-
-app.use('/api/users', userRouters);
-app.use('/api/measurements', measurementRoutes);
-
 const db = require('./config/db');
 
 db.query("SELECT 1", (err, result) => {
     if (err) {
-        console.error(" Database connection failed:", err);
+        console.error("Database connection failed:", err);
     } else {
         console.log(" Connected to MySQL database!");
     }
-});
-
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger-output.json');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
