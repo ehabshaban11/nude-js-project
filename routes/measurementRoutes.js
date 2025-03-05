591d5e237a218a5032714d9d9d61ecde1e08ce95
ehabshaban11 <141747535+ehabshaban11@users.noreply.github.com>
2025-03-06 01:47:06 +0200

    Update measurementRoutes.js
    "הוספת ניתובים לניהול מדידות לחץ דם - יצירה ושליפה"

--- routes/measurementRoutes.js
+++ routes/measurementRoutes.js
@@ -1,4 +1,22 @@
+
 const express = require('express');
 const router = express.Router();
+const MeasurementMiddleware = require('../middleware/measurementMiddleware');
+
+router.post('/', [MeasurementMiddleware.AddMeasurement], (req, res) => {
+    if (req.success) {
+        res.status(200).json({ msg: "ok", Last_Id: req.insertId });
+    } else {
+        res.status(500).json({ message: "Error adding measurement" });
+    }
+});
+
+router.get('/:user_id', [MeasurementMiddleware.GetMeasurements], (req, res) => {
+    if (req.success) {
+        res.status(200).json({ msg: "ok", data: req.measurements });
+    } else {
+        res.status(500).json({ message: "Error retrieving measurements" });
+    }
+});
 
 module.exports = router;
