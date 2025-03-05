a061786567c570d81ddb7bc2d892a4bdf01d50f7
ehabshaban11 <141747535+ehabshaban11@users.noreply.github.com>
2025-03-06 01:32:17 +0200

    Update userRoutes.js
    "יצירת קובץ ניתוב למדידות"

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,20 +2,4 @@ const express = require('express');
 const router = express.Router();
 const UserMiddleware = require('../middleware/userMiddleware');
 
-router.post('/', [UserMiddleware.AddUser], (req, res) => {
-    if (req.success) {
-        res.status(200).json({ msg: "ok", Last_Id: req.insertId });
-    } else {
-        res.status(500).json({ message: "Error adding user" });
-    }
-});
 
-router.get('/', [UserMiddleware.GetUsers], (req, res) => {
-    if (req.success) {
-        res.status(200).json({ msg: "ok", data: req.users });
-    } else {
-        res.status(500).json({ message: "Error retrieving users" });
-    }
-});
-
-module.exports = router;
